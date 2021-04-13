0fcfff01d44556b56f0d4a7f6879cb5cbe209f22
atif <ab5143@hotmail.com>
2021-04-13 18:28:41 -0400

    Initial Commit

---
+++ CricketerStats/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CricketerStats.MvcApplication" Language="C#" %>
---
+++ CricketerStats/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CricketerStats.Startup))]
+namespace CricketerStats
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
